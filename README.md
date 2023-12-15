0d778fa6dae47cd727ae05c336de6e3be8d80d86
stiffmeds <6158160+stiffmeds@users.noreply.github.com>
2023-12-15 19:28:35 +0800

    Add project files.

---
+++ README.md
@@ -0,0 +1,25 @@
+# Yoggest
+
+Makes Yogger's character model (and, optionally, other characters) scale with Vitality charges.
+
+## Installation (manual)
+
+1. Install the [BepInEx pack](https://across-the-obelisk.thunderstore.io/package/BepInEx/BepInExPack_AcrossTheObelisk/).
+2. Click _Manual Download_ at the top of the page.
+3. In Steam, right-click Across the Obelisk and select _Manage_->_Browse local files_.
+4. Extract the archive into the game folder. 
+5. Run the game. 
+
+## Installation (automatic)
+
+1. Download and install [Thunderstore Mod Manager](https://www.overwolf.com/app/Thunderstore-Thunderstore_Mod_Manager) or [r2modman](https://across-the-obelisk.thunderstore.io/package/ebkr/r2modman/).
+2. Click **Install with Mod Manager** button on top of the page.
+3. Run the game via the mod manager.
+
+## Support
+
+Open a github issue or make a post in the **modding #support-and-requests** channel of the [official Across the Obelisk Discord](https://discord.gg/across-the-obelisk-679706811108163701).
+
+## Donations
+
+I make mods because I love it, not because I want to make money from it. If you really, really want to donate to me, my preferred non-profit organisations are [Greyhound Adoptions WA](https://greyhoundadoptionswa.com.au/donation/) ([paypal](https://www.paypal.com/donate?token=m8DwEGGEH0FFsS6PS-5p4MX9_5g8_ocMMrNFjaELN-xcG6Ok-KCFabu5xtB-57QBiOM7QLSuKVUepvL_)) or [Headache Australia](https://headacheaustralia.org.au/donate/).
---
+++ Yoggest.cs
@@ -0,0 +1,63 @@
+﻿using BepInEx;
+using HarmonyLib;
+// #TODO: post-Essentials update using static Obeliskial_Essentials.Essentials;
+using BepInEx.Logging;
+using UnityEngine;
+using BepInEx.Configuration;
+
+namespace Yoggest
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    // #TODO: post-Essentials update [BepInDependency("com.stiffmeds.obeliskialessentials")]
+    [BepInProcess("AcrossTheObelisk.exe")]
+    public class Yoggest : BaseUnityPlugin
+    {
+        public static ConfigEntry<float> medsYoggestIncreasePerVitality { get; private set; }
+        public static ConfigEntry<bool> medsYoggestEveryone { get; private set; }
+        public static ConfigEntry<bool> medsYoggestEveryoneIncludingThem { get; private set; }
+        internal const int ModDate = 20231215;
+        private readonly Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+        internal static ManualLogSource Yog;
+        private void Awake()
+        {
+            Yog = Logger;
+            medsYoggestIncreasePerVitality = Config.Bind(new ConfigDefinition("Settings", "Increase per Vitality stack"), 1f, new ConfigDescription("X% increase in Yogger's model size for each Vitality stack."));
+            medsYoggestEveryone = Config.Bind(new ConfigDefinition("Settings", "Not Just Yogger"), false, new ConfigDescription("Let heroes other than Yogger scale with Vitality stacks."));
+            medsYoggestEveryoneIncludingThem = Config.Bind(new ConfigDefinition("Settings", "Not Just You"), false, new ConfigDescription("Let NPCs scale with Vitality stacks."));
+            Yog.LogInfo($"{PluginInfo.PLUGIN_GUID} {PluginInfo.PLUGIN_VERSION} is loaded!");
+            harmony.PatchAll();
+        }
+    }
+    [HarmonyPatch]
+    internal class Patches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Character), "GetMaxHP")]
+        public static void GetMaxHPPostfix(ref Character __instance)
+        {
+            if (__instance != null && __instance.HeroData != null && __instance.HeroData.HeroSubClass != null && __instance.Alive && __instance.HeroItem != null && __instance.HeroItem.animatedTransform != null)
+            { // it's a hero...
+                string subclassID = __instance.HeroData.HeroSubClass.Id;
+                /* #TODO: post-Essentials update
+                if (subclassID == "medsdlctwo")
+                    subclassID = medsCloneTwo;
+                else if (subclassID == "medsdlcthree")
+                    subclassID = medsCloneThree;
+                else if (subclassID == "medsdlcfour")
+                    subclassID = medsCloneFour;*/
+                if (subclassID == "bandit" || Yoggest.medsYoggestEveryone.Value)
+                { // it's yogger (soon to be yoggest)!
+                    float sizeIncrease = 1f + ((float)__instance.GetAuraCharges("vitality") * 0.01f * Yoggest.medsYoggestIncreasePerVitality.Value);
+                    __instance.HeroItem.animatedTransform.localScale = new Vector3(sizeIncrease, sizeIncrease, __instance.HeroItem.animatedTransform.localScale.z);
+                    Yoggest.Yog.LogDebug("Setting " + subclassID + " size to: " + sizeIncrease.ToString());
+                }
+            }
+            else if (Yoggest.medsYoggestEveryoneIncludingThem.Value && __instance != null && __instance.NpcData != null && __instance.Alive && __instance.NPCItem != null && __instance.NPCItem.animatedTransform != null)
+            {
+                float sizeIncrease = 1f + ((float)__instance.GetAuraCharges("vitality") * 0.01f * Yoggest.medsYoggestIncreasePerVitality.Value);
+                __instance.NPCItem.animatedTransform.localScale = new Vector3(sizeIncrease, sizeIncrease, __instance.NPCItem.animatedTransform.localScale.z);
+                Yoggest.Yog.LogDebug("Setting " + __instance.NpcData.Id + " size to: " + sizeIncrease.ToString());
+            }
+        }
+    }
+}
